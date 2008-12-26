bf2e77693325403e21d4142237b2d392fc02646e
jharai0815 <jharai0815@gmail.com>
2008-12-26 11:12:23 +0900

    [refactor] to add "caret" functionality
    darcs-hash:20081226021223-c02dc-bbac367d198ff9e83d7546c8daeb2f6f97ba5da6

--- Text/RegexC.hs
+++ Text/RegexC.hs
@@ -1,61 +1,65 @@
 {-# OPTIONS -fglasgow-exts #-}
 module Text.RegexC where
  
-newtype Regex a = Rx { runRegex :: (String -> [(String, a)]) }
---newtype RxInput = RxIn { rxInToScan :: String, rxInScanned :: String }
+newtype Regex a = Rx { runRegex :: (RxTarget -> [(RxTarget, a)]) }
+    
+-- rxTargetScanned holds scanned chars in reverse order
+data RxTarget = RxTarget { rxTargetToScan :: String, rxTargetScanned :: String }
 
 instance Monad Regex where
-    m >>= next = Rx $ \str -> let
-                           thisCand = runRegex m str
-                           getNextCand (str', a) = runRegex (next a) str'
+    m >>= next = Rx $ \target -> let
+                           thisCand = runRegex m target
+                           getNextCand (target', a) = runRegex (next a) target'
                       in concatMap getNextCand thisCand
-    return a = Rx $ \str -> [(str, a)]
+    return a = Rx $ \target -> [(target, a)]
 
 rxOneChar :: Char -> Regex Char
-rxOneChar c = Rx $ \str -> case str of
-                         s : xs -> if c == s then [(xs, s)] else []
-                         [] -> []
+rxOneChar c = Rx $ \target -> case target of
+                         RxTarget (x : xs) ys -> if c == x
+                                                then [(RxTarget xs (x : ys), x)]
+                                                else []
+                         _ -> []
 
 -- http://www.haskell.org/ghc/docs/latest/html/users_guide/other-type-extensions.html#scoped-type-variables
+-- returns matched string and an arbitrary return value, which is Nothing if zero-matched.
 rxZeroOrMoreLongest :: forall a . Regex a -> Regex (String, Maybe a)
 rxZeroOrMoreLongest sub =
-    Rx $ \str -> let
-        getCand' :: [(String, (String, Maybe a))] -> [(String, (String, Maybe a))]
-        getCand' ((str', (matchedStr, _)) : _) =
-            map (\(s, a) -> (s, (matchedStr ++ getConsumed str' s, Just a))) $ runRegex sub str'
+    Rx $ \target -> let
+        getCand' :: [(RxTarget, (String, Maybe a))] -> [(RxTarget, (String, Maybe a))]
+        getCand' ((target', (matchedStr, _)) : _) =
+            map (\(t, a) -> (t, (matchedStr ++ getConsumed target' t, Just a))) $ runRegex sub target'
         getCand' [] = []
 
-        getCand :: [(String, (String, Maybe a))]
+        getCand :: [(RxTarget, (String, Maybe a))]
         getCand = reverse $
                   map head $
                   takeWhile (not . null) $
-                  iterate getCand' [(str, ("", Nothing))]
+                  iterate getCand' [(target, ("", Nothing))]
         in getCand
 
 rxOne :: forall a . Regex a -> Regex (String, a)
 rxOne sub =
-    Rx $ \str -> let
-        getCand' :: [(String, a)] -> [(String, (String, a))]
-        getCand' = map (\(str', a) -> (str', (getConsumed str str', a)))
+    Rx $ \target -> let
+        getCand' :: [(RxTarget, a)] -> [(RxTarget, (String, a))]
+        getCand' = map (\(target', a) -> (target', (getConsumed target target', a)))
 
-        getCand :: [(String, (String, a))]
-        getCand = getCand' $ runRegex sub str
+        getCand :: [(RxTarget, (String, a))]
+        getCand = getCand' $ runRegex sub target
         in getCand
 
 rxEnd :: Regex ()
-rxEnd = Rx $ \str -> case str of
-                       [] -> [("", ())]
+rxEnd = Rx $ \target -> case target of
+                       RxTarget [] ys -> [(RxTarget [] ys, ())]
                        _ -> []
 
 -- 暫定的なコード
-getConsumed :: String -> String -> String
-getConsumed before after = reverse $ drop (length after) $ reverse before
+getConsumed :: RxTarget -> RxTarget -> String
+getConsumed (RxTarget before _) (RxTarget after _) = reverse $ drop (length after) $ reverse before
 
 regexMatch :: Regex a -> String -> Maybe String
-regexMatch rx str = case runRegex (rxOne rx) str of
+regexMatch rx str = case runRegex (rxOne rx) (RxTarget str []) of
                       [] -> Nothing
-                      x : _ -> Just (fst $ snd x)
+                      (_, (matchedStr, _)) : _ -> Just matchedStr
 
--- hoge
 -- regexReplace :: Regex a -> String -> String
 -- regexReplace
