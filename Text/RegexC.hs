98ed689fff972403a2ee837ef02e1f0599f3c251
jharai0815 <jharai0815@gmail.com>
2008-12-26 21:21:30 +0900

    [refactor] improved readability
    darcs-hash:20081226122130-c02dc-9cdb4900a7ba6270b6eaa6a754e6252f3948a473

--- Text/RegexC.hs
+++ Text/RegexC.hs
@@ -4,10 +4,11 @@ module Text.RegexC where
 newtype Regex a = Rx { runRegex :: (RxTarget -> [(RxTarget, a)]) }
     
 -- rxTargetScanned and rxCurrentMatch hold scanned and matched chars in reverse order
+-- (reverse rxTargetScanned t) ++ (reverse rxCurrentMatch t) ++ (rxTargetToScan t) == targetStr
 data RxTarget = RxTarget {
-    rxTargetToScan :: String,
     rxTargetScanned :: String,
-    rxCurrentMatch :: String }
+    rxCurrentMatch :: String,
+    rxTargetToScan :: String }
 
 instance Monad Regex where
     m >>= next = Rx $ \target -> let
@@ -19,9 +20,9 @@ instance Monad Regex where
 rxOneChar :: Char -> Regex Char
 rxOneChar c = Rx $ \target ->
     case target of
-        RxTarget (x : toScan) scanned matched ->
+        RxTarget scanned matched (x : toScan) ->
             if c == x
-            then [(RxTarget toScan (x : scanned) (x : matched), x)]
+            then [(RxTarget (x : scanned) (x : matched) toScan, x)]
             else []
         _ -> []
 
@@ -29,50 +30,50 @@ rxOneChar c = Rx $ \target ->
 -- returns matched string and an arbitrary return value, which is Nothing if zero-matched.
 rxStar :: forall a . Regex a -> Regex (String, Maybe a)
 rxStar sub =
-    Rx $ \(RxTarget toScan scanned _) -> let
+    Rx $ \(RxTarget scanned _ toScan) -> let
         getCand'' :: String -> (RxTarget, a) -> (RxTarget, (String, Maybe a))
-        getCand'' prevMatch (RxTarget toScan' scanned' matched', a) =
-            (RxTarget toScan' scanned' matched, (reverse matched, Just a))
+        getCand'' prevMatch (RxTarget scanned' matched' toScan', a) =
+            (RxTarget scanned' matched toScan', (reverse matched, Just a))
             where
                 matched = matched' ++ prevMatch
 
         getCand' :: [(RxTarget, (String, Maybe a))] -> [(RxTarget, (String, Maybe a))]
-        getCand' ((target@(RxTarget _ _ matched), _) : _) =
+        getCand' ((target@(RxTarget _ matched _), _) : _) =
             map (getCand'' matched) $ runRegex sub target
         getCand' [] = []
 
         getCand :: [(RxTarget, (String, Maybe a))]
         getCand =
             reverse $ map head $ takeWhile (not . null) $
-            iterate getCand' [(RxTarget toScan scanned [], ("", Nothing))]
+            iterate getCand' [(RxTarget scanned [] toScan, ("", Nothing))]
         in getCand
 
 rxParenthesis :: forall a . Regex a -> Regex (String, a)
-rxParenthesis sub = Rx $ \(RxTarget toScan scanned _)  -> let
+rxParenthesis sub = Rx $ \(RxTarget scanned _ toScan)  -> let
     getCand' :: [(RxTarget, a)] -> [(RxTarget, (String, a))]
-    getCand' = map (\(target'@(RxTarget _ _ matched), a) -> (target', (reverse matched, a)))
+    getCand' = map (\(target'@(RxTarget _ matched _), a) -> (target', (reverse matched, a)))
 
     getCand :: [(RxTarget, (String, a))]
-    getCand = getCand' $ runRegex sub $ RxTarget toScan scanned []
+    getCand = getCand' $ runRegex sub $ RxTarget scanned [] toScan
     in getCand
 
 rxCaret :: Regex ()
 rxCaret = Rx $ \target ->
     case target of
-        RxTarget toScan [] matched -> [(RxTarget toScan [] matched, ())]
+        RxTarget [] matched toScan -> [(RxTarget [] matched toScan, ())]
         _ -> []
 
 rxDollar :: Regex ()
 rxDollar = Rx $ \target ->
     case target of
-        RxTarget [] scanned matched -> [(RxTarget [] scanned matched, ())]
+        RxTarget scanned matched [] -> [(RxTarget scanned matched [], ())]
         _ -> []
 
 regexMatch :: Regex a -> String -> Maybe String
 regexMatch rx str =
-    case runRegex (rxParenthesis rx) (RxTarget str [] []) of
+    case runRegex (rxParenthesis rx) (RxTarget [] [] str) of
         [] -> Nothing
-        (RxTarget _ _ matched, _) : _ -> Just (reverse matched)
+        (RxTarget _ matched _, _) : _ -> Just (reverse matched)
 
 -- regexReplace :: Regex a -> String -> String
 -- regexReplace
