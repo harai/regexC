480837a380e4a6fb8585e535551f1cff06a32740
jharai0815 <jharai0815@gmail.com>
2008-12-26 13:16:13 +0900

    [refactor] added flexibility
    darcs-hash:20081226041613-c02dc-bd21fb9703f3615be8262b04856d0de08c2f4881

--- Text/RegexC.hs
+++ Text/RegexC.hs
@@ -4,68 +4,76 @@ module Text.RegexC where
 newtype Regex a = Rx { runRegex :: (RxTarget -> [(RxTarget, a)]) }
     
 -- rxTargetScanned holds scanned chars in reverse order
-data RxTarget = RxTarget { rxTargetToScan :: String, rxTargetScanned :: String }
+data RxTarget = RxTarget {
+    rxTargetToScan :: String,
+    rxTargetScanned :: String,
+    rxCurrentMatch :: String }
 
 instance Monad Regex where
     m >>= next = Rx $ \target -> let
-                           thisCand = runRegex m target
-                           getNextCand (target', a) = runRegex (next a) target'
-                      in concatMap getNextCand thisCand
+        thisCand = runRegex m target
+        getNextCand (target', a) = runRegex (next a) target'
+        in concatMap getNextCand thisCand
     return a = Rx $ \target -> [(target, a)]
 
 rxOneChar :: Char -> Regex Char
-rxOneChar c = Rx $ \target -> case target of
-                         RxTarget (x : xs) ys -> if c == x
-                                                then [(RxTarget xs (x : ys), x)]
-                                                else []
-                         _ -> []
+rxOneChar c = Rx $ \target ->
+    case target of
+        RxTarget (x : xs) ys matched ->
+            if c == x
+            then [(RxTarget xs (x : ys) (matched ++ [x]), x)]
+            else []
+        _ -> []
 
 -- http://www.haskell.org/ghc/docs/latest/html/users_guide/other-type-extensions.html#scoped-type-variables
 -- returns matched string and an arbitrary return value, which is Nothing if zero-matched.
 rxZeroOrMoreLongest :: forall a . Regex a -> Regex (String, Maybe a)
 rxZeroOrMoreLongest sub =
-    Rx $ \target -> let
+    Rx $ \(RxTarget toScan scanned _) -> let
+        getCand'' :: String -> (RxTarget, a) -> (RxTarget, (String, Maybe a))
+        getCand'' prevMatch (RxTarget toScan' scanned' matched', a) =
+            (RxTarget toScan' scanned' matched, (matched, Just a))
+            where
+                matched = prevMatch ++ matched'
+
         getCand' :: [(RxTarget, (String, Maybe a))] -> [(RxTarget, (String, Maybe a))]
-        getCand' ((target', (matchedStr, _)) : _) =
-            map (\(t, a) -> (t, (matchedStr ++ getConsumed target' t, Just a))) $ runRegex sub target'
+        getCand' ((target@(RxTarget _ _ matched), _) : _) =
+            map (getCand'' matched) $ runRegex sub target
         getCand' [] = []
 
         getCand :: [(RxTarget, (String, Maybe a))]
         getCand = reverse $
                   map head $
                   takeWhile (not . null) $
-                  iterate getCand' [(target, ("", Nothing))]
+                  iterate getCand' [(RxTarget toScan scanned [], ("", Nothing))]
         in getCand
 
 rxOne :: forall a . Regex a -> Regex (String, a)
-rxOne sub =
-    Rx $ \target -> let
-        getCand' :: [(RxTarget, a)] -> [(RxTarget, (String, a))]
-        getCand' = map (\(target', a) -> (target', (getConsumed target target', a)))
+rxOne sub = Rx $ \(RxTarget toScan scanned _)  -> let
+    getCand' :: [(RxTarget, a)] -> [(RxTarget, (String, a))]
+    getCand' = map (\(target'@(RxTarget _ _ matched), a) -> (target', (matched, a)))
 
-        getCand :: [(RxTarget, (String, a))]
-        getCand = getCand' $ runRegex sub target
-        in getCand
+    getCand :: [(RxTarget, (String, a))]
+    getCand = getCand' $ runRegex sub $ RxTarget toScan scanned []
+    in getCand
 
 rxCaret :: Regex ()
 rxCaret = Rx $ \target ->
     case target of
-        RxTarget xs [] -> [(RxTarget xs [], ())]
+        RxTarget xs [] matched -> [(RxTarget xs [] matched, ())]
         _ -> []
 
 rxEnd :: Regex ()
-rxEnd = Rx $ \target -> case target of
-                       RxTarget [] ys -> [(RxTarget [] ys, ())]
-                       _ -> []
-
--- 暫定的なコード
-getConsumed :: RxTarget -> RxTarget -> String
-getConsumed (RxTarget before _) (RxTarget after _) = reverse $ drop (length after) $ reverse before
+rxEnd = Rx $ \target ->
+    case target of
+        RxTarget [] ys matched -> [(RxTarget [] ys matched, ())]
+        _ -> []
 
 regexMatch :: Regex a -> String -> Maybe String
-regexMatch rx str = case runRegex (rxOne rx) (RxTarget str []) of
-                      [] -> Nothing
-                      (_, (matchedStr, _)) : _ -> Just matchedStr
+regexMatch rx str =
+    case runRegex (rxOne rx) (RxTarget str [] []) of
+        [] -> Nothing
+        (RxTarget _ _ matchedStr, _) : _ -> Just matchedStr
 
 -- regexReplace :: Regex a -> String -> String
 -- regexReplace
